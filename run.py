b1dce719df3df85981454f702bf4009a0ce90f4a
Alexandra Skyslakova <xskyslak@fi.muni.cz>
2022-11-18 10:30:43 +0100

    Test

---
+++ run.py
@@ -0,0 +1,50 @@
+"""
+    Start Application Sequence:
+    1) bind sockets for flask to bokeh communications
+    2) start bokeh server (Tornado) running bokeh bkapp
+    3) start flask server (Tornado) running flask app
+"""
+import time
+import logging
+
+from threading import Thread
+
+import index
+from server import (
+    bk_worker,
+    get_sockets
+)
+from config import (
+    BOKEH_URL,
+    FLASK_URL
+)
+
+logging.basicConfig(level=logging.INFO)
+
+def run():
+    """Run flask application
+
+    """
+    log = logging.getLogger(__name__)
+
+    # get sockets, so bkapp and app can talk
+    bk_sockets, bk_port = get_sockets()
+
+    # start bokeh sever
+    thread_bokeh = Thread(target=bk_worker, args=[bk_sockets, bk_port], daemon=True)
+    thread_bokeh.start()
+
+    bokeh_url = BOKEH_URL.replace('$PORT', str(bk_port))
+    log.info("Bokeh Server App Running at: %s", bokeh_url)
+
+    # start flask server
+    thread_flask = Thread(target=index.start_tornado, daemon=True)
+    thread_flask.start()
+
+    log.info("Flask + Bokeh Server App Running at: %s", FLASK_URL)
+
+    # loop for ever
+    while True:
+        time.sleep(0.01)
+
+run()
